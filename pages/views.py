00cdbb9a722bc905995a424167ef4e3353155d69
linearza <vanwijk.mc@gmail.com>
2024-08-11 16:32:59 +0200

    chore: adds basic template

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request, *args, **kwargs):
   print(request.user, args, kwargs)
-  return HttpResponse("<h1>Hello world</h1>")
+  return render(request, "home.html", {})
+  # return HttpResponse("<h1>Hello world</h1>")
 
 def contact_view(*args, **kwargs):
   return HttpResponse("<h1>Contact us</h1>")
